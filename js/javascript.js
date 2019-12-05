152d9a94ab91c541d899790e299265c3d50db5ad
Maxym <mak7_07@ukr.net>
2019-12-05 19:06:10 +0200

    work..

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+// let images = [
+// 'img/baner.png',
+// 'img/baner2.png'
+// ];
+// let num = 0;
+// function next() {
+// 	let slider = 
+// 	document.getElementById('slider');
+// 	num++;
+// 	if(num >= images.length){
+// 		num = 0;
+// 	}
+// 	slider. src = images[num];
+// }
+// function prev() {
+// 	let slider = 
+// 	document.getElementById('slider');
+// 	num--;
+// 	if(num <0){ 
+// 		num = images.length-1;
+// 	}
+// 	slider. src = images[num];
+// }
